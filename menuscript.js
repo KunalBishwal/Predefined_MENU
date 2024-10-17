68cd7d109437d80bbf9cfefa857efd497c7fec15
KunalBishwal <159259649+KunalBishwal@users.noreply.github.com>
2024-10-18 00:49:40 +0530

    Create menuscript.js

---
+++ menuscript.js
@@ -0,0 +1,31 @@
+
+// MOBILE MENU OFF CANVAS 
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "80%";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+
+
+// STICKY MENU ON THE TOP 
+
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
